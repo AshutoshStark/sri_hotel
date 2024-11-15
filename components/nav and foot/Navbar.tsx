23751ac44f88d7454deca640b4e782d6fbc3650a
Ashutosh Kumar <ashu28jan2003@gmail.com>
2024-11-15 23:12:44 +0530

    update

--- components/nav and foot/Navbar.tsx
+++ components/nav and foot/Navbar.tsx
@@ -2,31 +2,23 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useState } from 'react'
-import { CiSearch } from 'react-icons/ci'
-import { HiUser } from 'react-icons/hi'
-import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io'
 
 const Navbar = () => {
 
   const [toggle,setToggle] = useState<boolean>(false)
-  const {padEnd} = usePathname()
-
+  const pathname = usePathname()
+  console.log(pathname)
   return (
     <div className='text-2xl text-othertext font-semibold flex gap-14 items-center justify-between'>
       <Link href={'/'}>
         <img src="/loog.png" alt="" />
       </Link>
       <div className='flex gap-11 items-center'>
-      <Link href={'/booking'} className={``}>Book Room</Link>
-      <Link href={'/trip'}>Book Trip</Link>
+      <Link href={'/'} className={pathname === '/'? 'decoration-1 text-prime underline underline-offset-[12px] underline-prime' : ''}>Home</Link>
+      <Link href={'/booking'} className={pathname === '/booking'? 'decoration-1 text-prime underline underline-offset-[12px] underline-prime' : ''}>Book Room</Link>
+      <Link href={'/trip'} className={pathname === '/trip'? 'decoration-1 text-prime underline underline-offset-[12px] underline-prime' : ''}>Book Trip</Link>
       </div>
       <div onClick={()=>setToggle(!toggle)} className='flex-row'>
-      {/* <div className='text-primetext bg-othertext flex gap-4 px-4 py-2 rounded-xl'><HiUser /> {toggle ? <IoIosArrowUp /> : <IoIosArrowDown />}</div>
-     {toggle && <div className='text-lg items-start gap-6 flex-row absolute py-3 px-2 w-1/12 font-normal bg-black rounded-lg mt-2'>
-        <p className=''>Profile</p>
-        <p className='my-8'>Dashboard</p>
-        <p>My Store</p>
-      </div>} */}
       </div>
     </div>
   )
