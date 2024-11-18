94bae337e0b3c0c99834d1c36cfcb0af54b05b01
Ashutosh Kumar <ashu28jan2003@gmail.com>
2024-11-18 10:37:50 +0530

    Update page.tsx

--- app/trip-details/[place]/[price]/[duration]/page.tsx
+++ app/trip-details/[place]/[price]/[duration]/page.tsx
@@ -7,14 +7,22 @@ const page = () => {
   const params = useParams();
   const {place, price, duration} = params;
 
-  let priced = price.replace('%2C','')
+  let priced: any;
+  let pricen: any;
+  let pricef: any;
+
+  if (typeof price === 'string') {
+    priced = price.replace('%2C', '');
+  } else {
+    throw new Error("Invalid type for 'day'. Expected a string.");
+  }
 
   return (
     <div className='flex flex-col justify-center items-start py-12 gap-6'>
       <input type="text" name="" id="" className='bg-transparent border border-gray-700 rounded-md py-2 px-6' placeholder='Your Name'/>
       <input type="text" name="" id="" className='bg-transparent border border-gray-700 rounded-md py-2 px-6' placeholder='Email'/>
       <input type="text" name="" id="" className='bg-transparent border border-gray-700 rounded-md py-2 px-6' placeholder='Number'/>
-      <p>Room Details : {decodeURI(place)}</p>
+      <p>Room Details : {place && typeof place === "string" ? decodeURI(place) : "Unknown"}</p>
       <p>Room Details : {priced}</p>
       <p>Trip Duration : {duration}</p>
     </div>
